ef20df2cb6132c5980eaffb29e35c3ccc9c032f8
Maria Eduarda <malvesduarte444@gmail.com>
2023-12-15 11:50:43 -0300

    terminando mod

---
+++ Importação/main.js
@@ -0,0 +1,9 @@
+const {gets, print} = require("./aula");
+
+const numerosSorteados = [];
+for(let i = 0; i < 5; i++){
+    const numerosSorteados = gets();
+    numerosSorteados.push(numerosSorteados);
+}
+
+print(numerosSorteados);
